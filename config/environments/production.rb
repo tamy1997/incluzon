d6913d20785a13e269f155bf9991ddeda7741ef2
Hitesh Kanwar <hiteshgurjar786@gmail.com>
2021-11-25 11:56:01 +0530

    add email cred to production.rb file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,6 +96,17 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_mailer.delivery_method = :smtp
+ # host = 'example.com' #replace with your own url
+  config.action_mailer.default_url_options = { host: 'https://incluzon.herokuapp.com/' }
+   config.action_mailer.smtp_settings = {
+   :address              => "smtp.gmail.com",
+   :port                 => 587,
+   :user_name            => "tanmayyuvasoft325@gmail.com",
+   :password             => "Tj@12345",
+   :authentication       => "plain",
+   :enable_starttls_auto => true
+ }
 
   # Inserts middleware to perform automatic connection switching.
   # The `database_selector` hash is used to pass options to the DatabaseSelector
