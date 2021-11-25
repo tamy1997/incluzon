667f499be72f188cc0ada5b230e4ff226d8f31a2
Hitesh Kanwar <hiteshgurjar786@gmail.com>
2021-11-25 12:07:12 +0530

    change smtp setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -98,7 +98,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.delivery_method = :smtp
  # host = 'example.com' #replace with your own url
-  config.action_mailer.default_url_options = { host: 'incluzon.herokuapp.com/' }
+  config.action_mailer.default_url_options = { host: 'incluzon.herokuapp.com' }
    config.action_mailer.smtp_settings = {
    :address              => "smtp.gmail.com",
    :port                 => 587,
@@ -107,6 +107,7 @@ Rails.application.configure do
    :authentication       => "plain",
    :enable_starttls_auto => true
  }
+
 
   # Inserts middleware to perform automatic connection switching.
   # The `database_selector` hash is used to pass options to the DatabaseSelector
